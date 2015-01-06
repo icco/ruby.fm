5cea3a3305bd76c6fdbfc2535f08dd8fb63c9ac6
Nat Welch <nat@natwelch.com>
2015-01-06 08:56:41 +0000

    put css in css, stylesheets is too long of a word

--- lib/scss_initializer.rb
+++ lib/scss_initializer.rb
@@ -5,8 +5,12 @@ module ScssInitializer
     # Store SCSS files (by default) within 'app/stylesheets'.
     require 'sass/plugin/rack'
     Sass::Plugin.options[:syntax] = :scss
-    Sass::Plugin.options[:template_location] = Padrino.root("app/stylesheets")
-    Sass::Plugin.options[:css_location] = Padrino.root("public/stylesheets")
+    Sass::Plugin.options[:always_update]     = (Padrino.env == :development)
+    Sass::Plugin.options[:css_location]      = Padrino.root("public/css")
+    Sass::Plugin.options[:full_exception]    = (Padrino.env == :development)
+    Sass::Plugin.options[:never_update]      = (Padrino.env == :production)
+    Sass::Plugin.options[:style]             = :compact
+    Sass::Plugin.options[:template_location] = Padrino.root("app/css")
     app.use Sass::Plugin::Rack
   end
 end
