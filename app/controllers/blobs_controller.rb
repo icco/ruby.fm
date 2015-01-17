f94f9434fa0ce64f8d43a982c6d612f70dbd6d1b
Nat Welch <nat@natwelch.com>
2015-01-17 23:04:14 +0000

    I have no idea what I'm failing at.

--- app/controllers/blobs_controller.rb
+++ app/controllers/blobs_controller.rb
@@ -1,9 +1,16 @@
 class BlobsController < ApplicationController
   def new
+    @bucket = "http://#{S3_DIR.key}.s3.amazonaws.com/"
     headers = {}
-    options = { path_style: true, acl: :public_read, success_action_status: 201, success_action_redirect: "#{request.host_with_port}/success" }
-    path = "#{SecureRandom.uuid}/${filename}"
-    @url = S3.put_object_url(ENV['S3_BUCKET'], path, Chronic.parse('next week'), headers, options)
+    options = { path_style: true, acl: :public_read, success_action_status: 201 }
+    expires = Chronic.parse('in 10 minutes').to_i
+    @path = "/#{SecureRandom.uuid}/${filename}"
+    @url = S3.signed_url(options.merge({
+      :bucket_name => S3_DIR.key,
+      :object_name => @path,
+      :method   => 'POST',
+      :headers  => headers,
+    }), expires)
     @blob = Blob.new
   end
 end
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,12 +1,33 @@
 # Be sure to restart your server when you modify this file.
 options = {
   :provider => 'AWS',
-  :aws_access_key_id =>  ENV['AWS_ACCESS_KEY_ID'].strip,
-  :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'].strip,
+  :aws_access_key_id =>  ENV['AWS_ACCESS_KEY_ID'].strip.delete('"\''),
+  :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'].strip.delete('"\''),
+  :region => ENV['S3_REGION'].strip.delete('"\''),
 }
-p options
 S3 = Fog::Storage.new(options)
-S3_DIR = S3.directories.create(
-  :key => ENV['S3_BUCKET'].strip,
-  :public => true
-)
+S3_DIR = S3.directories.get(ENV['S3_BUCKET'].strip.delete('"\''))
+if S3_DIR.nil?
+  S3_DIR = S3.directories.create(
+    :key => ENV['S3_BUCKET'].strip.delete('"\''),
+    :public => true
+  )
+end
+
+if Rails.env.development?
+  host = "http://localhost:3000"
+elsif Rails.env.production?
+  host = "https://ruby.fm"
+end
+cors =  {
+  'CORSConfiguration' => [
+    {
+      'AllowedOrigin' => host,
+      'AllowedMethod' => ['POST', 'GET',  'PUT'],
+      'AllowedHeader' => '*',
+      'MaxAgeSeconds' => 3000
+    }
+  ]
+}
+S3.put_bucket_cors(S3_DIR.key, cors)
+p S3.get_bucket_cors(S3_DIR.key)
