c3b36d5cba5f2c7cde59fac9fff945436df69b5e
Nat Welch <nat@natwelch.com>
2015-02-21 15:46:20 +0000

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 20150221154202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authorizations", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blobs", force: :cascade do |t|
     t.integer  "user_id"
@@ -37,6 +45,12 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "show_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
