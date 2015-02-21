fccd5eab2f6adb88cdbbe46b5ee5c661d4b8599a
Nat Welch <nat@natwelch.com>
2015-02-21 16:07:48 +0000

    comment

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -3,6 +3,16 @@ class Authorization < ActiveRecord::Base
   validates_presence_of :user_id, :uid, :provider
   validates_uniqueness_of :uid, :scope => :provider
 
+  # Given a login hash (example below), find a valid authorization.
+  #
+  # {"utf8"=>"✓",
+  #  "authenticity_token"=>"bBvR1G6VMxGs+3bppidps1+ANTTjy9AnXPoDz7y3lK1uhvYx26zz7s2oyff+U778MmrND5ar4iCKC9dQ6tmEQA==",
+  #  "name"=>"icco",
+  #  "email"=>"nat@natwelch.com",
+  #  "password"=>"[FILTERED]",
+  #  "password_confirmation"=>"[FILTERED]",
+  #  "commit"=>"Register",
+  #  "provider"=>"identity"}
   def self.find_from_hash(hash)
     find_by_provider_and_uid(hash['provider'], hash['uid'])
   end
