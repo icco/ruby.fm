b06652b8bf007540943c71a295619f852e361986
Nat Welch <nat@natwelch.com>
2015-03-01 10:55:52 +0000

    simpler

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -2,5 +2,5 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   # A UID is actually an email address for identity, which is the name of the
   # omniauth provider for logging in with emails and passwords
   validates_uniqueness_of :uid
-  validates_format_of :uid, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates_format_of :uid, :with => /@/
 end
