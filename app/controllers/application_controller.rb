349816d52e799641f9e0f16e04af6d011788486f
Matthew Johnston <warmwaffles@gmail.com>
2015-03-12 20:34:07 -0500

    Render access denied if not authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from(Pundit::NotAuthorizedError, with: :access_denied)
+
+  def access_denied(error=nil)
+    respond_to do |format|
+      format.html do
+        render("shared/access_denied")
+      end
+    end
+  end
 end
