cbf2441572cd1b1b59756a06a8debfe76793510d
Alex Baldwin <alexbaldwin@gmail.com>
2015-05-24 15:32:49 -0700

    Sort DESC episodes

--- app/controllers/my/episodes_controller.rb
+++ app/controllers/my/episodes_controller.rb
@@ -1,7 +1,7 @@
 module My
   class EpisodesController < AuthorizedController
     def index
-      @episodes = current_user.episodes.recent.includes(:channel)
+      @episodes = current_user.episodes.recent.includes(:channel).order(created_at: :desc)
 
       respond_to do |format|
         format.html
