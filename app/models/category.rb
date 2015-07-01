b659a0c42d1241f4fcde009f139ed6425e5d10f4
Matthew Johnston <warmwaffles@gmail.com>
2015-06-30 21:07:41 -0500

    Category select fields now work

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,6 +9,26 @@ class Category
     all.select { |c| c.child? }
   end
 
+  def self.grouped
+    hash = {}
+    all.each do |category|
+      if category.root?
+        if category.children?
+          hash[category.name] = []
+        else
+          hash[category.name] = [category.name]
+        end
+      else
+        hash[category.parent.name] << category.name
+      end
+    end
+    hash
+  end
+
+  def self.find(name)
+    all.find { |c| c.name == name }
+  end
+
   def initialize(name, parent=nil)
     @name = name
     @parent = parent
@@ -20,6 +40,10 @@ class Category
 
   def child?
     !root?
+  end
+
+  def children?
+    CATEGORIES.any? { |c| c.parent == self }
   end
 
   def to_s
