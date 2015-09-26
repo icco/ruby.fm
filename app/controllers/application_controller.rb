0495a638356b1e5e3039fa38ce1aed9d0b448873
Matthew Johnston <warmwaffles@gmail.com>
2015-09-26 17:42:51 -0500

    Rescue from not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
     rescue_from(Pundit::NotAuthorizedError, with: :access_denied)
     rescue_from(Pundit::NotDefinedError, with: :server_error)
     rescue_from(ActionController::UnknownFormat, with: :unknown_format)
+    rescue_from(ActiveRecord::RecordNotFound, with: :not_found)
   end
 
   def primary_channel
@@ -20,6 +21,16 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :primary_channel
+
+  def not_found(error)
+    respond_to do |format|
+      format.html do
+        flash[:alert] = I18n.t('errors.not_found')
+        redirect_to(root_url)
+      end
+      format.any { redirect_to(root_url) }
+    end
+  end
 
   def unknown_format(error=nil)
     respond_to do |format|
