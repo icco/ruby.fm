93393512778d2a29eeb8079e9f1b0dff72aeb088
Alex Baldwin <alexbaldwin@gmail.com>
2015-09-30 11:39:24 -0700

    Trying out stack overflow things

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  # https://stackoverflow.com/questions/27495326/rare-error-about-missing-view-welcome-index-formats-image
+  respond_to :html
+
   def index
   end
 
