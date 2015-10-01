b95aba119c03a9b4a32d66ac3662c308ab3cb3bd
Alex Baldwin <alexbaldwin@gmail.com>
2015-10-01 13:04:00 -0700

    Random stack overflow test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   respond_to :html
 
   def index
+    # https://stackoverflow.com/questions/27495326/rare-error-about-missing-view-welcome-index-formats-image
+    respond_with()
   end
 
   def itunes
