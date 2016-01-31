603194895dca9dc0c6d43f290a046ad90e6ed912
Alex Baldwin <alexbaldwin@gmail.com>
2016-01-30 16:55:04 -0800

    Updating syntax for secure token

--- config/initializers/imgix.rb
+++ config/initializers/imgix.rb
@@ -2,8 +2,7 @@ module Imgix
   def self.client
     @client ||= Imgix::Client.new(
       host: ENV['IMGIX_URL'],
-      token: ENV['IMGIX_TOKEN'],
-      secure: true
+      secure_url_token: ENV['IMGIX_TOKEN']
     )
   end
 end
