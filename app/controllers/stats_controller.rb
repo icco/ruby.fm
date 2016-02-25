f90bab5cf234ad7d5c3ba87bc64f92259ddf864b
Matthew Johnston <warmwaffles@gmail.com>
2016-02-24 23:25:25 -0600

    Silly baldwin, you want both 30 days

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,7 +5,7 @@ class StatsController < AuthorizedController
     @channel = primary_channel
     @episodes = @channel.episodes
 
-    @episode_stats = fetch_plays_in_last_7_days(@channel)
+    @episode_stats = fetch_plays_grouped_by_episode(@channel)
 
     respond_to do |format|
       format.html
@@ -16,15 +16,15 @@ class StatsController < AuthorizedController
   def overall
     @channel = primary_channel
 
-    @data = fetch_plays_last_30_days(@channel)
+    @data = fetch_plays_overall(@channel)
 
     respond_to do |format|
       format.json
     end
   end
 
-  def fetch_plays_last_30_days(channel)
-    Rails.cache.fetch("#{channel.id}/last-30-days", expires_in: 12.hours) do
+  def fetch_plays_overall(channel)
+    Rails.cache.fetch("#{channel.id}/plays-overall", expires_in: 12.hours) do
       Keen.count_unique("podcast.download", {
         interval: "daily",
         timeframe: "previous_30_days",
@@ -39,8 +39,8 @@ class StatsController < AuthorizedController
   end
 
 
-  def fetch_plays_in_last_7_days(channel)
-    Rails.cache.fetch("#{channel.id}/last-7-days", expires_in: 12.hours) do
+  def fetch_plays_grouped_by_episode(channel)
+    Rails.cache.fetch("#{channel.id}/plays-grouped-by-episode", expires_in: 12.hours) do
       hash = {}
       Keen.count("podcast.download", {
         group_by: "episode_id",
@@ -51,7 +51,7 @@ class StatsController < AuthorizedController
             property_value: channel.id
           }
         ],
-        timeframe: "this_7_days"
+        timeframe: "previous_30_days"
       }).each do |data|
         hash[data['episode_id']] = data['result']
       end
