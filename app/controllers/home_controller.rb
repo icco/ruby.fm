933ab410ecbee2ee684b6c873947926f2b589512
Alex Baldwin <alexbaldwin@gmail.com>
2016-05-30 09:50:24 -0700

    Visible episodes bug fix, safari format bug fix, and undos root rerouting for logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to channel_path(primary_channel)
-    end
-
     @channel = Channel.find_by_slug('consolefm')
 
     if @channel
