0bf708bc7b2b1a54d7cd86ce552187c31d84618b
Alex Baldwin <alexbaldwin@gmail.com>
2017-05-29 15:09:33 -0700

    Stripe and Intercome Forest integrations

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,2 +1,14 @@
 ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
 ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+
+ForestLiana.integrations = {
+  intercom: {
+    app_id: ENV['INTERCOM_APP_ID'],
+    api_key: ENV['INTERCOM_API_KEY'],
+    mapping: 'User'
+  },
+  stripe: {
+    api_key: ENV['STRIPE_SECRET_KEY'],
+    mapping: 'User.stripe_id'
+  }
+}
