5c6059ef641b0a52a41174f1bdf280ba88c2bf7f
Matthew Johnston <warmwaffles@gmail.com>
2017-05-30 18:57:27 -0500

    Make bugsnag configurable via ENV vars

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,5 @@
 Bugsnag.configure do |config|
-  config.api_key = "c9102b5f2ad913675fd4265529d2c187"
-  config.notify_release_stages = ['production', 'staging']
+  config.api_key = ENV["BUGSNAG_API_KEY"]
+  config.release_stage = (ENV["BUGSNAG_RELEASE_STAGE"] || Rails.env)
+  config.notify_release_stages = %w(production staging)
 end
